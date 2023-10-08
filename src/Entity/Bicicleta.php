bdf53b998a6ec710911c225c88ec86f286a3e4ee
J3ffn <jefferson.mangueira12@gmail.com>
2023-10-08 17:48:01 -0300

    add: Entidade de bicicleta

---
+++ src/Entity/Bicicleta.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BicicletaRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BicicletaRepository::class)]
+class Bicicleta
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private int $id;
+
+    public function __construct(
+        #[ORM\Column(length: 60)]
+        private string $nome,
+
+        #[ORM\Column]
+        private $preco,
+
+        #[ORM\Column(length: 255)]
+        private string $descricao,
+
+        #[ORM\Column(length: 30)]
+        private string $nomeImagem
+    ) {
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): static
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function getPreco(): float
+    {
+        return $this->preco;
+    }
+
+    public function setPreco(float $preco): static
+    {
+        $this->preco = $preco;
+
+        return $this;
+    }
+
+    public function getDescricao(): string
+    {
+        return $this->descricao;
+    }
+
+    public function setDescricao(string $descricao): static
+    {
+        $this->descricao = $descricao;
+
+        return $this;
+    }
+
+    public function getNomeImagem(): string
+    {
+        return $this->nomeImagem;
+    }
+
+    public function setNomeImagem(string $nomeImagem): static
+    {
+        $this->nomeImagem = $nomeImagem;
+
+        return $this;
+    }
+}
